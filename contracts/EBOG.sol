3e9bbadc5eece333b142a4dfbe3d3c37e2960a47
stinkyfi <83800142+stinkyfi@users.noreply.github.com>
2021-07-04 02:25:51 -0700

    Update EBOG.sol
    Updating supply

--- contracts/EBOG.sol
+++ contracts/EBOG.sol
@@ -6,6 +6,6 @@ import "ERC20.sol";
 
 contract STINK is ERC20 {
     constructor() public ERC20("EBOG Digital Venue", "EBOG"){
-        _mint(msg.sender, 1000000000  * (10 ** uint256(decimals())));
+        _mint(msg.sender, 100000000  * (10 ** uint256(decimals())));
     }
 }
