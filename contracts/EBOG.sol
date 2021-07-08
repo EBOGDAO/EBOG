cb60b2710d0c4d44ce3eba5cbb6310a08c5fcfc9
stinkyfi <83800142+stinkyfi@users.noreply.github.com>
2021-07-07 19:30:33 -0700

    Update EBOG.sol
    Added single and multiple airdrop functionality. Tested with 774 addresses.
    
    https://rinkeby.etherscan.io/tx/0xe1624d9f13ddbcfd705e1f4cef4b7b1983c36b6e1fb6b446b0d0b92b0f849f5f

--- contracts/EBOG.sol
+++ contracts/EBOG.sol
@@ -5,7 +5,27 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EBOG is ERC20 {
-    constructor() public ERC20("EBOG Digital Venue", "EBOG"){
-        _mint(msg.sender, 100000000  * (10 ** uint256(decimals())));
+    
+    uint256 decimal = 10 ** uint256(decimals());
+    address[] accounts;
+    uint256[] amounts;
+    
+    constructor() ERC20("EBOG Digital Arena", "EBOG") {
+       minterGoBrrr(100000000 * decimal);
+    }
+
+    function massAirdrop(address[] memory _accounts, uint256  _amount) public {
+        for (uint256 i = 0; i < _accounts.length; i++) {
+            ERC20.transfer(_accounts[i], _amount * decimal);
+        }
+    }
+    
+    function airdrop(address _account, uint256 _amount) public {
+        ERC20.transfer(_account, _amount * decimal);
+    }
+    
+    function minterGoBrrr(uint256 _toMint) internal
+    {
+        _mint(msg.sender, _toMint);
     }
 }
